10321d6a742014769505b1ca7c14e32709292dca
Alex Siegman <asiegman@gmail.com>
2014-10-22 00:47:36 -0500

    Add some meta options for prettier adminage

--- roster/models.py
+++ roster/models.py
@@ -5,6 +5,10 @@ class EQClass(models.Model):
     name = models.TextField()
     short_name = models.TextField()
 
+    class Meta:
+        verbose_name = 'Class'
+        verbose_name_plural = 'Classes'
+
     def __str__(self):
         return self.name
 
@@ -12,6 +16,9 @@ class EQClass(models.Model):
 class EQRace(models.Model):
     name = models.TextField()
     short_name = models.TextField()
+
+    class Meta:
+        verbose_name = 'Race'
 
     def __str__(self):
         return self.name
@@ -22,6 +29,9 @@ class EQCharacter(models.Model):
     surname = models.TextField(blank=True)
     eqclass = models.ForeignKey(EQClass)
     eqrace = models.ForeignKey(EQRace)
+
+    class Meta:
+        verbose_name = 'Character'
 
     def __str__(self):
         return self.name
