d1cf55db375f644267bd8b08610178cc6ee60513
Alex Siegman <asiegman@gmail.com>
2018-05-08 22:46:36 -0500

    Fix spacing, names

--- dkp/models.py
+++ dkp/models.py
@@ -3,22 +3,25 @@ from eq.models import Event, Item
 
 
 class Raid(models.Model):
-	date = models.DateTimeField()
-	event = models.ForeignKey(Event, on_delete=models.CASCADE)
-	attendance_value = models.FloatField()
+    date = models.DateTimeField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    attendance_value = models.FloatField()
 
-	def loot(self):
-		return Loot.objects.filter(raid = self.id)
+    def loot(self):
+        return Loot.objects.filter(raid=self.id)
 
-	def __str__(self):
-		return "{} {}".format(self.date, self.event.name)
+    # The amount of DKP the raid is worth.
+    def dkp(self):
+        return sum([l.dkp for l in self.loot()])
 
+    def __str__(self):
+        return "{} {}".format(self.date, self.event.name)
 
 
 class Loot(models.Model):
-	raid = models.ForeignKey(Raid, on_delete=models.CASCADE)
-	item = models.ForeignKey(Item, on_delete=models.CASCADE)
-	dkp_value = models.FloatField()
+    raid = models.ForeignKey(Raid, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    dkp = models.FloatField()  # The amount of DKP the item was sold for
 
-	def __str__(self):
-		return "{} {}".format(self.raid, self.item)
+    def __str__(self):
+        return "{} {}".format(self.raid, self.item)
