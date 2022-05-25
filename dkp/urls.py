98076386badfba812633030c97b3199151b1cc5f
Alex Siegman <asiegman@gmail.com>
2022-05-26 05:49:13 +0900

    Upgrade to Django 4

--- dkp/urls.py
+++ dkp/urls.py
@@ -1,14 +1,13 @@
 """
 URLs for DKP app
 """
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = "dkp"
 
 urlpatterns = [
-    url(r"^$", views.RaidsView.as_view(), name="index"),
+    re_path(r"^$", views.RaidsView.as_view(), name="index"),
     path("raids/add/", views.RaidCreateView.as_view(), name="raid-add"),
     path("raids/<pk>/", views.RaidDetailView.as_view(), name="raid-view"),
 ]
